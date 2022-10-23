cbcc4d094156f023991060442aa544531b991be2
Jia Xu <iamjiax@gmail.com>
2022-10-22 23:50:04 -0700

    a6-fix a href errors

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -21,19 +21,19 @@ const ExploreComponent = () => {
 
                 <ul className="nav mt-2 mb-2 nav-tabs">
                     <li className="nav-item">
-                        <a className="nav-link active">For You</a>
+                        <a href="javascript:void(0)" className="nav-link active">For You</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link">Trending</a>
+                        <a href="javascript:void(0)" className="nav-link">Trending</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link">News</a>
+                        <a href="javascript:void(0)" className="nav-link">News</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link">Sports</a>
+                        <a href="javascript:void(0)" className="nav-link">Sports</a>
                     </li>
                     <li className="nav-item d-none d-md-inline-block">
-                        <a className="nav-link">Entertainment</a>
+                        <a href="javascript:void(0)" className="nav-link">Entertainment</a>
                     </li>
                 </ul>
 
